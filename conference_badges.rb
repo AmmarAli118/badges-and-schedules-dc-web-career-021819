9cbbccf681f5a1c08e3322de4fa6d511bd245870
Ammar Ali <funnyguy118@gmail.com>
2019-01-24 21:01:29 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -19,7 +19,8 @@ def assign_rooms (attendees)
 end
 
 def printer(attendees)
-
-   batch_badge_creator(attendees)
-   assign_rooms(attendees)
+  batch_badge_creator(attendees).each do |element|
+    puts element
+  end
+  #assign_rooms(attendees)
 end
