932d35a82a9d6fc3be9a52e578d854339e6559da
Ammar Ali <funnyguy118@gmail.com>
2019-01-24 20:46:14 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,7 +13,7 @@ end
 def assign_rooms (attendees)
   room_assignments = []
   attendees.each_with_index { |item , index|
-    room_assignments[item] = "Hello, #{item}! You'll be assigned to room #{index.to_s}!"
+    room_assignments[item] = "Hello, #{item}! You'll be assigned to room #{index}!"
   }
   room_assignments
 end
