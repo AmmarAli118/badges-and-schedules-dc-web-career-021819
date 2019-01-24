a6b419a879f567896d4304620ee9feb99d802bbd
Ammar Ali <funnyguy118@gmail.com>
2019-01-24 20:40:23 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,22 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  badge_messages = []
+  attendees.each do |attendee|
+    badge_messages.push(badge_maker(attendee))
+  end
+  badge_messages
+end
+
+def assign_rooms (attendees)
+  room_assignments = []
+  attendees.each_with_index { |item , index|
+    room_assignments[item] = "Hello, #{room_assignments[item]}! You'll be assigned to room #{index}!"
+  }
+  room_assignments
+end
+
+def printer
+end
